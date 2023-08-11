52deac7e64abee3f7d8cb45412ae43e36b7db1d1
Ocean-Eyes <68937084+Ocean-Eyes@users.noreply.github.com>
2023-08-11 20:34:08 +0900

    Figure out how several functions work

---
+++ BasicExercise_py/CameraCalibration.py
@@ -0,0 +1,33 @@
+import numpy as np
+import cv2 as cv
+import glob
+
+if __name__ == "__main__":
+    # termination criteria
+    criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+    # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+    objp = np.zeros((6*9,3), np.float32)
+    objp[:,:2] = np.mgrid[0:9,0:6].T.reshape(-1,2)
+
+    # Arrays to store object points and image points from all the images.
+    objpoints = [] # 3d point in real world space
+    imgpoints = [] # 2d points in image plane.
+    img = cv.imread("C:\\Users\\Autonav\\github_repo\\ComputerVision\\BasicExercise_py\\chessboard.jpg")
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+    # Find the chess board corners
+    ret, corners = cv.findChessboardCorners(gray, (9,6), None)
+
+    # If found, add object points, image points (after refining them)
+    if ret == True:
+        objpoints.append(objp)
+        corners2 = cv.cornerSubPix(gray, corners, (11,11), (-1,-1), criteria)
+        imgpoints.append(corners2)
+
+    # Draw and display the corners
+    cv.drawChessboardCorners(img, (9,6), corners2, ret)
+    cv.imshow('img', img)
+    
+    cv.waitKey()
+    cv.destroyAllWindows()
